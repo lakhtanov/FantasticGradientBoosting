a1860e7e42909b72240876d1bd89e2d4c9ef645c
Roman Aleksandrov <rialeksandrov@gmail.com>
2018-04-08 12:06:58 +0300

    Fix style guide issues[2]

--- utils/data_containers/ElementContainer.cpp
+++ utils/data_containers/ElementContainer.cpp
@@ -3,33 +3,35 @@
 #include <cassert>
 
 namespace utils {
-    namespace data_containers {
-        ElementContainer::ElementContainer(const std::string &str)
-                : raw_data_(str), type_(DataType::String) {
-            try {
-                double el = std::stod(raw_data_);
-                type_ = DataType::Double;
 
-            } catch (...) {
-                // TODO(rialeksandrov) Debug Output here.
-            }
-        }
+namespace data_containers {
 
-        bool ElementContainer::IsDouble() {
-            return type_ == DataType::Double;
-        }
+ElementContainer::ElementContainer(const std::string &str)
+    : raw_data_(str), type_(DataType::String) {
+  try {
+    double el = std::stod(raw_data_);
+    type_ = DataType::Double;
+  } catch (...) {
+    // TODO(rialeksandrov) Debug Output here.
+  }
+}
 
-        bool ElementContainer::IsString() {
-            return type_ == DataType::String;
-        }
+bool ElementContainer::IsDouble() {
+  return type_ == DataType::Double;
+}
 
-        double ElementContainer::GetDouble() {
-            assert(IsDouble());
-            return std::stod(raw_data_);
-        }
+bool ElementContainer::IsString() {
+  return type_ == DataType::String;
+}
 
-        std::string ElementContainer::GetString() {
-            return raw_data_;
-        }
-    } // namespace data_containers
-} // namespace utils
+double ElementContainer::GetDouble() {
+  assert(IsDouble());
+  return std::stod(raw_data_);
+}
+
+std::string ElementContainer::GetString() {
+  return raw_data_;
+}
+
+}  // namespace data_containers
+}  // namespace utils
--- utils/data_containers/ElementContainer.h
+++ utils/data_containers/ElementContainer.h
@@ -1,25 +1,28 @@
-#ifndef FANTASTICGRADIENTBOOSTING_ELEMENTCONTAINER_H
-#define FANTASTICGRADIENTBOOSTING_ELEMENTCONTAINER_H
+#ifndef UTILS_DATA_CONTAINERS_ELEMENTCONTAINER_H_
+#define UTILS_DATA_CONTAINERS_ELEMENTCONTAINER_H_
 
 #include <string>
 
 namespace utils {
-    namespace  data_containers {
-        class ElementContainer {
-        public:
-            enum class DataType {Double, String, LongInteger};
-            explicit ElementContainer (const std::string& str);
-            bool IsDouble();
-            bool IsString();
-            double GetDouble();
-            std::string GetString();
 
-        private:
-            std::string raw_data_;
-            DataType type_;
-        };
-    } // namespace data_containers
-} // namespace utils
+namespace  data_containers {
+
+class ElementContainer {
+ public:
+  enum class DataType {Double, String, LongInteger};
+  explicit ElementContainer(const std::string& str);
+  bool IsDouble();
+  bool IsString();
+  double GetDouble();
+  std::string GetString();
+
+ private:
+  std::string raw_data_;
+  DataType type_;
+};
+
+}  // namespace data_containers
+}  // namespace utils
 
 
-#endif //FANTASTICGRADIENTBOOSTING_ELEMENTCONTAINER_H
+#endif  // UTILS_DATA_CONTAINERS_ELEMENTCONTAINER_H_
