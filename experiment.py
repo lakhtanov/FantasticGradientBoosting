46e2d9555fe1d4af7ef56dbd8b40d47308cf4478
Ivan Lakhtanov <lakhtanov@yandex-team.ru>
2018-05-12 22:03:50 +0300

    Fix scipt codestyle

--- experiment.py
+++ experiment.py
@@ -1,9 +1,10 @@
 import argparse
 import json
 import os
+from os.path import realpath, dirname, join
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from os.path import realpath, dirname, join
+
 
 def main():
     parser = argparse.ArgumentParser(description="Experiment preparator and run")
@@ -15,25 +16,31 @@ def main():
         df = pd.read_csv(config["Experiment"]["Data"])
         change_targets(df, config["Experiment"]["TargetValueName"])
         y = df[:][ [config["Experiment"]["IdName"], config["Experiment"]["TargetValueName"]] ]
+
         X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.3)
         X_test.drop(config["Experiment"]["TargetValueName"], axis = 1)
+
         print(X_train.shape, y_train.shape)
         print(X_test.shape, y_test.shape)
+
         train = realpath(join(dirname(config["Experiment"]["Data"]), "sliced_train.csv"))
         test = realpath(join(dirname(config["Experiment"]["Data"]), "sliced_test.csv"))
         test_y = realpath(join(dirname(config["Experiment"]["Data"]), "sliced_test_y.csv"))
+
         config_path = realpath(args.config)
         print (test, config_path)
+
         config["Experiment"]["TrainData"] = train
         config["Experiment"]["TestData"] = test
         config["Experiment"]["TestDataLabels"] = test_y
+
         os.system("rm " + test_y)
         os.system("rm " + test)
         os.system("rm " + train)
+
         X_train.to_csv(train, header=True, index=None, sep=',', float_format='%.3f')
         X_test.to_csv(test, header=True, index=None, sep=',', float_format='%.3f')
         y_test.to_csv(test_y, header=True, index=None, sep=',', mode='a')
-
 
     with open(args.config, "w") as file:
         json.dump(config, file, sort_keys=True, indent = 4)
