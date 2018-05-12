e081b3771536085a4a75ee53a665514fc82c9e4e
Roman Aleksandrov <rialeksandrov@gmail.com>
2018-05-12 21:55:37 +0300

    small fixes

--- analyse.py
+++ analyse.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn import metrics
 
 def main():
-    parser = argparse.ArgumentParser(description="Experiment preparator and run")
+    parser = argparse.ArgumentParser(description="Experiment analyser")
     parser.add_argument("config", type=str, help="path to json defining the experiment")
     args = parser.parse_args()
     with open(args.config, "r") as file:
@@ -17,11 +17,9 @@ def main():
         d1 = {}
         d2 = {}
         for index, object in y.iterrows():
-            #print(index, object)
             d1[int(object[id])] = object[target]
         for index, object in result.iterrows():
             d2[int(object[id])] = object[target]
-        #print(result)
         ys = []
         predicted = []
         for key in d1:
@@ -29,9 +27,8 @@ def main():
             predicted.append(d2[key])
         predicted = np.array(predicted).astype(float)
         ys = np.array(ys).astype(int)
-        #print (predicted[0:10], ys[0:10])
         fpr, tpr, thresholds = metrics.roc_curve(ys, predicted, pos_label=1)
-        print(metrics.auc(fpr, tpr))
+        print("AUC score is: ", metrics.auc(fpr, tpr))
 
 
 if __name__ == "__main__":
